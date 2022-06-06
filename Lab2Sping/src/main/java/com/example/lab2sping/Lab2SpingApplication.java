d80dfa5baf799c219287aae58c5c51617bdd503c
Nalexx6 <nikita2002oleksienko@gmail.com>
2022-06-06 14:57:38 +0300

    Lab2 initial

---
+++ Lab2Sping/src/main/java/com/example/lab2sping/Lab2SpingApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab2sping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2SpingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2SpingApplication.class, args);
+    }
+
+}
